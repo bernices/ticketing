14e7dec56873d0f73532a88b2eb8fa4411cee6d0
BS <joeyshek2189@live.hk>
2023-01-02 02:33:45 +0800

    Split the app and index for testing

---
+++ auth/src/app.ts
@@ -0,0 +1,29 @@
+import express from "express";
+import "express-async-errors";
+import { json } from "body-parser";
+import { currentuserRouter } from "./routes/current-user";
+import { signinRouter } from "./routes/signin";
+import { signoutRouter } from "./routes/signout";
+import { signupRouter } from "./routes/signup";
+import { errorHandler } from "./middlewares/error-handler";
+import { NotFoundError } from "./errors/not-found-error";
+import cookieSession from "cookie-session";
+const app = express();
+app.set("trust proxy", true);
+app.use(json());
+app.use(
+  cookieSession({
+    signed: false, //not encrypt
+    secure: true, //must use https
+  })
+);
+app.use(currentuserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
+app.all("*", async () => {
+  throw new NotFoundError();
+});
+app.use(errorHandler);
+
+export { app };
