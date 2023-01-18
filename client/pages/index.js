8e4f53a6e4c631ef47c77440866255e7dbab64ca
BS <joeyshek2189@live.hk>
2023-01-19 00:56:32 +0800

    use getServerSideProps

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,28 @@
-const LandingPage = () => {
+import axios from "axios";
+
+const LandingPage = ({ currentUser }) => {
+  // console.log(currentUser);
+  // axios.get('/api/users/currentuser').catch((err) => {
+  //   console.log(err.message);
+  // });
+
   return <h1>Landing Page</h1>;
 };
+
+// This gets called on every request
+export async function getServerSideProps() {
+  // Fetch data from external API
+  const { data } = await axios.get(
+    "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser",
+    {
+      headers: {
+        Host: "ticketing.dev", //for ingress-nginx to know what service and port to go
+      },
+    }
+  );
+  // Pass data to the page via props
+  console.log(data);
+  return { props: { currentuser: data } };
+}
 
 export default LandingPage;
