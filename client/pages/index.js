1d16ed89be1652d13d2dba9b857cea529f9ed7f6
BS <joeyshek2189@live.hk>
2023-01-22 03:55:51 +0800

    add header component

--- client/pages/index.js
+++ client/pages/index.js
@@ -2,20 +2,21 @@ import buildClient from "../api/build-client";
 
 const LandingPage = ({ currentUser }) => {
   console.log(currentUser);
-  // axios.get('/api/users/currentuser').catch((err) => {
-  //   console.log(err.message);
-  // });
-
-  return <h1>Landing Page</h1>;
+  return currentUser ? (
+    <h1>You are signed in </h1>
+  ) : (
+    <h1>You are not signed in</h1>
+  );
 };
 
 // This gets called on every request
 export async function getServerSideProps(context) {
   // Fetch data from external API
+  console.log("test index js");
   const client = buildClient(context);
   const { data } = await client.get("/api/users/currentuser");
   // Pass data to the page via props
-  return { props: { currentUser: data } };
+  return { props: data };
 }
 
 export default LandingPage;
