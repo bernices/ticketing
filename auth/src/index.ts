3fd72825478d01288bc2bebb6b9a29d061690695
BS <joeyshek2189@live.hk>
2023-06-18 04:11:52 +0800

    adding start up msg

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 import { app } from "./app";
 const start = async () => {
+  console.log("Starting up");
   // a check that env variable is defined
   if (!process.env.JWT_KEY) {
     throw new Error("JWT_KEY must defined");
