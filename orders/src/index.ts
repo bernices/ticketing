067110fd772f37ea73b318f6544d497282f88d10
BS <joeyshek2189@live.hk>
2023-06-19 00:19:45 +0800

    add start up msg to orders service

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -8,7 +8,9 @@ import { ExpirationCompleteListener } from "./events/listener/expiration-complet
 import { PaymentCreatedListener } from "./events/listener/payment-created-listener";
 
 const start = async () => {
+  console.log("starting...");
   // a check that env variable is defined
+
   if (!process.env.JWT_KEY) {
     throw new Error("JWT_KEY must defined");
   }
